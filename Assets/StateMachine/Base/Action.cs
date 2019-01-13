d7aafd77fa5afbadf1a23efc67c732bc75de8be3
willlauer <wlauer@stanford.edu>
2019-01-13 15:37:05 -0800

    Added skeleton behaviour classes + fixed swinging force bug

--- Assets/StateMachine/Base/Action.cs
+++ Assets/StateMachine/Base/Action.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Action : ScriptableObject {
+public abstract class Action : ScriptableObject 
+{
     public abstract void Act (StateController controller);
 }
--- Assets/StateMachine/Input_.cs
+++ Assets/StateMachine/Input_.cs
@@ -27,6 +27,7 @@ public class Input_ {
 		public static KeyStatus Dash = new KeyStatus(KeyCode.J);
 		public static KeyStatus Attack = new KeyStatus(KeyCode.K);
 		public static KeyStatus Grab = new KeyStatus(KeyCode.H);
+		public static KeyStatus Swing = new KeyStatus(KeyCode.L);
 
 		// Used externally once set
 		public static KeyStatus MoveX = new KeyStatus(null);
@@ -39,7 +40,8 @@ public class Input_ {
 		private static KeyStatus _Up = new KeyStatus(KeyCode.W);
 		private static KeyStatus _Down = new KeyStatus(KeyCode.S);
 
-		private List<KeyStatus> li = new List<KeyStatus>() { Jump, Dash, Attack, Grab, _Left, _Right, _Up, _Down };
+		// List of all user-handled keys 
+		private List<KeyStatus> li = new List<KeyStatus>() { Jump, Dash, Attack, Grab, _Left, _Right, _Up, _Down, Swing };
 
 
 		public void PrintInputState()
@@ -52,6 +54,7 @@ public class Input_ {
 			Debug.Log("_Right: " 	+ (_Right.Pressed ? 1 : 0) + " " 	+ (_Right.Down ? 1 : 0));
 			Debug.Log("_Up: " 		+ (_Up.Pressed ? 1 : 0) + " " 		+ (_Up.Down ? 1 : 0));
 			Debug.Log("_Down: " 	+ (_Down.Pressed ? 1 : 0) + " " 	+ (_Down.Down ? 1 : 0));
+			Debug.Log("Swing: " 	+ (Swing.Pressed ? 1 : 0) + " " 	+ (Swing.Down ? 1 : 0));
 
 		}		
 
@@ -67,7 +70,8 @@ public class Input_ {
                 {"_Left", _Left},
                 {"_Right", _Right},
                 {"_Up", _Up},
-                {"_Down", _Down}
+                {"_Down", _Down},
+				{"Swing", Swing}
             };
         }
 
@@ -90,6 +94,9 @@ public class Input_ {
                 key.Down = Input.GetKeyDown(k);
 				key.Pressed = Input.GetKey(k);
 			}
+
+			// Some of our KeyStatus objects are used for the value field, which is determined
+			// by other key status objects
 			MoveX.Value = (_Left.Pressed ? -1 : 0) + (_Right.Pressed ? 1 : 0);
 			MoveY.Value = (_Down.Pressed ? -1 : 0) + (_Right.Pressed ? 1 : 0);
 			DeltaRope.Value = (_Down.Pressed ? -1 : 0) + (_Right.Pressed ? 1 : 0);
